7307e191955abf849999ac479d8dd319611c3119
Vishal <vishal.neharkar@brij.tech>
2023-04-02 17:17:43 +0530

    example setup

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import { globSync } from 'glob';
 
 import * as middlewares from './middlewares';
-import api from './api';
-import MessageResponse from './interfaces/MessageResponse';
-
-require('dotenv').config();
+import helloWorld from './api';
 
 const app = express();
 
@@ -16,13 +17,30 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
-});
+app.use('/', helloWorld);
 
-app.use('/api/v1', api);
+const routes = globSync('**/*.json', {
+  ignore: [
+    'package.json',
+    'package-lock.json',
+    'npm-shrinkwrap.json',
+    'tsconfig.json',
+    'node_modules/**',
+  ],
+})
+  .map(file => JSON.parse(fs.readFileSync(path.resolve(file), 'utf8')));
+
+for (const route of routes) {
+  let method = String(route.method).toLowerCase() as keyof typeof app;
+
+  app[method](
+    route.path,
+    (req: express.Request, res: express.Response) => {
+      const firstResponse = route.responses[0];
+
+      return res.json(firstResponse);
+    });
+}
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
